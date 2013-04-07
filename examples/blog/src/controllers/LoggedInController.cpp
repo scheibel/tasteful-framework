e23d50c94e2f8b767f88c5ffea17d5f90c867b06
Willy Scheibel <willyscheibel@gmx.de>
2013-04-07 12:56:26 +0200

    redirect to home in blog example on unauthorized access

--- examples/blog/src/controllers/LoggedInController.cpp
+++ examples/blog/src/controllers/LoggedInController.cpp
@@ -1,9 +1,17 @@
 #include <controllers/LoggedInController>
 
+#include <RedirectException>
+#include <controllers/HomeController>
+
 LoggedInController::LoggedInController() : BlogController() {
 	
 }
 
-bool LoggedInController::isAuthorized() {
+bool LoggedInController::isAuthorized() const {
 	return (bool)getSession()->author;
+}
+
+void LoggedInController::unauthorized()
+{
+	throw RedirectException(url(&HomeController::index));
 }
